7b860a8ccdd3fce7812b4b0b131078b04e0a639b
Karthik Anant <karthika25@gmail.com>
2017-03-13 23:02:02 -0400

    initial unit test that kinda tests everything

--- test/SimpleRules.Tests/RuleGenerationTests.cs
+++ test/SimpleRules.Tests/RuleGenerationTests.cs
@@ -1,4 +1,9 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleRules.Attributes;
+using SimpleRules.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SimpleRules.Tests
 {
@@ -8,7 +13,19 @@ namespace SimpleRules.Tests
         [TestMethod]
         public void CanGenerateSimpleRules()
         {
-
+            var users = new List<User>
+            {
+                new User { Id = 1001, RegistrationDate = DateTime.Now, LastLoginDate = DateTime.Now.AddDays(-1) }
+            };
+            var results = new SimpleRulesEngine()
+                                .RegisterMetadata<User, UserMetadata>()
+                                .Validate<User>(users);
+            Assert.IsNotNull(results);
+            Assert.AreEqual(1, results.Count());
+            var first = results.First();
+            Assert.AreEqual(1001, first.Key);
+            Assert.AreEqual(1, first.Errors.Count);
+            Assert.AreEqual("Registration Date is less than Last Login Date for entity with Id 1001", first.Errors[0]);
         }
 
         [TestMethod]
@@ -16,5 +33,21 @@ namespace SimpleRules.Tests
         {
 
         }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public DateTime? LastLoginDate { get; set; }
+    }
+
+    public class UserMetadata
+    {
+        [EntityKey]
+        public int Id { get; set; }
+
+        [GreaterThan("RegistrationDate")]
+        public DateTime? LastLoginDate { get; set; }
     }
 }
